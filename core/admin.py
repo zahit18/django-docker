d21bfb209100ac20bb14da4f3202c1f4b1da49a7
zahit18 <zahithdez@gmail.com>
2025-05-29 20:39:44 -0700

    Add Models && Migrations

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,60 @@
 from django.contrib import admin
 
 # Register your models here.
+from core import models
+
+@admin.register(models.Bank)
+class BankAdmin(admin.ModelAdmin):
+    list_display = [
+        'bank_name',
+        'id',
+        'address',
+        'status',
+    ]
+
+@admin.register(models.AccountBank)
+class AccountBankAdmin(admin.ModelAdmin):
+    list_display = [
+        'account_name',
+        'id',
+        'bank',
+        'currency',
+        'status',
+    ]
+
+@admin.register(models.Currency)
+class CurrencyAdmin(admin.ModelAdmin):
+    list_display = [
+        'currency_name',
+        'id',
+        'short_name',
+        'status',
+    ]
+
+    
+@admin.register(models.CategoryProvider)
+class CategoryProviderAdmin(admin.ModelAdmin):
+    list_display = [
+        'category_name',
+        'id',
+        'status',
+    ]
+
+@admin.register(models.Provider)
+class ProviderAdmin(admin.ModelAdmin):
+    list_display = [
+        'provider_name',
+        'id',
+        'status',
+    ]
+
+@admin.register(models.Payment)
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = [
+        'payment_name',
+        'id',
+        'status',
+        'provider',
+        'account',
+        'timestamp'
+    ]
--- core/models.py
+++ core/models.py
@@ -1,3 +1,66 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Bank(models.Model):
+    bank_name = models.CharField(max_length=32, default='Generic Bank Name')
+    address = models.CharField(max_length=32, default='Generic Bank Address')
+    status = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.bank_name
+
+class Currency(models.Model):
+    currency_name = models.CharField(max_length=16, default='Generic Currency')
+    short_name = models.CharField(max_length=3, default='GC')
+    status = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.currency_name
+
+class AccountBank(models.Model):
+    account_name = models.CharField(max_length=16, default='Generic Account')
+    bank = models.ForeignKey(Bank, on_delete=models.CASCADE)
+    currency = models.ForeignKey(Currency, on_delete=models.CASCADE)
+    status = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.account_name
+
+class CategoryProvider(models.Model):
+    category_name = models.CharField(max_length=16, default='Generic Category Provider')
+    status = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.category_name
+
+class Provider(models.Model):
+    provider_name = models.CharField(max_length=32, default='Generic Provider')
+    category = models.ForeignKey(CategoryProvider, on_delete=models.CASCADE)
+    status = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.provider_name
+    
+class Payment(models.Model):
+    payment_name = models.CharField(max_length=32, default='Generic Payment')
+    account = models.ForeignKey(AccountBank, on_delete=models.CASCADE)
+    provider = models.ForeignKey(Provider, on_delete=models.CASCADE)
+    timestamp = models.DateField(auto_now_add=True)
+    status = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.payment_name
+
+"""
+Cuentas de banco
+    Bancos
+    Divisas
+
+Proveedores
+    Categorias
+
+Pagos
+    Cuentas
+    Proveedores
+"""
